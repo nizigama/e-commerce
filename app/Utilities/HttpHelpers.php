2e68453c4ce4befa2568a8510dff66228be94351
jean davy Nizigama <jedavy.n@gmail.com>
2022-11-10 16:21:05 +0200

    Fixed erronous implementation of helpers' file

--- app/Utilities/HttpHelpers.php
+++ app/Utilities/HttpHelpers.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace App\Utilities;
-
 use App\Exceptions\BadRequestException;
 use App\Exceptions\ForbiddenException;
 use App\Exceptions\NotFoundException;
@@ -9,29 +7,23 @@ use App\Exceptions\UnknownException;
 use Exception;
 use Illuminate\Http\Response;
 
-class HttpHelpers
+/**
+ *
+ * Retrieve the http message and status code for a given exception
+ *
+ * @return array
+ */
+function getHttpMessageAndStatusCodeFromException(Exception $exception)
 {
-
-    /**
-     *
-     * Retrieve the http message and status code for a given exception
-     *
-     * @return array
-     */
-
-
-    function getHttpMessageAndStatusCodeFromException(Exception $exception)
-    {
-        return [
-            $exception->getMessage(),
-            match (get_class($exception)) {
-                BadRequestException::class => Response::HTTP_BAD_REQUEST,
-                NotFoundException::class => Response::HTTP_NOT_FOUND,
-                ForbiddenException::class => Response::HTTP_FORBIDDEN,
-                UnknownException::class => Response::HTTP_INTERNAL_SERVER_ERROR,
-                default => Response::HTTP_INTERNAL_SERVER_ERROR
-            },
-            $exception->getCode(),
-        ];
-    }
+    return [
+        $exception->getMessage(),
+        match (get_class($exception)) {
+            BadRequestException::class => Response::HTTP_BAD_REQUEST,
+            NotFoundException::class => Response::HTTP_NOT_FOUND,
+            ForbiddenException::class => Response::HTTP_FORBIDDEN,
+            UnknownException::class => Response::HTTP_INTERNAL_SERVER_ERROR,
+            default => Response::HTTP_INTERNAL_SERVER_ERROR
+        },
+        $exception->getCode(),
+    ];
 }
